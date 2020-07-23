abe99940b49f54081e1d9012b06a81906d7a1d56
tuzup <s4sunnygeorge@gmail.com>
2020-07-23 11:54:26 +0530

    Test deployment

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+//const fs = require("fs");
+//const https = require("https");
+const connectDB = require("./config/db");
 
 const app = express();
 
-// Connect Database 
+// Connect Database
 connectDB();
 
-// Init Middleware 
-app.use(express.json({extended: false}));
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
-// Define Routes 
-app.use('/api/users', require('./routes/api/users'));
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+
+/* code dedicated to https
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./ssl/server.key"),
+      cert: fs.readFileSync("./ssl/server.cert"),
+    },
+    app
+  )
+  .listen(5000, function () {
+    console.log(
+      "Example app listening on port 3000! Go to https://localhost:3000/"
+    );
+  });*/
 
 const PORT = process.env.PORT || 5000;
 
